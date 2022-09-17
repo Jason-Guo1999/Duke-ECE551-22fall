a8383310c8f0f6b2087c93edd3e0cbcdc785fa30
zg105 <zg105@duke.edu>
2022-09-16 21:57:22 -0400

    didassignment 053

--- 053_sort_lines/sortLines.c
+++ 053_sort_lines/sortLines.c
@@ -14,8 +14,54 @@ void sortData(char ** data, size_t count) {
   qsort(data, count, sizeof(char *), stringOrder);
 }
 
+void Mysort(FILE * f) {
+  char ** lines = malloc(sizeof(*lines));
+  size_t lsize = 0;
+  size_t numOflines = 0;
+  char * l = 0;
+  while (getline(&l, &lsize, f) >= 0) {
+    lines = realloc(lines, (numOflines + 1) * (sizeof(*lines)));
+    lines[numOflines] = l;
+    numOflines++;
+    l = 0;
+  }
+  sortData(lines, numOflines);
+  for (int i = 0; i < (int)numOflines; i++) {
+    printf("%s\n", lines[i]);
+    free(lines[i]);
+  }
+  free(l);
+  free(lines);
+  return;
+}
 int main(int argc, char ** argv) {
   //WRITE YOUR CODE HERE!
+  if (argc == 1) {
+    if (stdin == NULL) {
+      fprintf(stderr, "falied to open stdin\n");
+      exit(EXIT_FAILURE);
+    }
+    FILE * f = stdin;
+    Mysort(f);
+    if (fclose(stdin) != 0) {
+      fprintf(stderr, "failed to close stdin\n");
+      exit(EXIT_FAILURE);
+    }
+  }
+  else {
+    for (int i = 0; i < argc - 1; i++) {
+      FILE * f = fopen(argv[i], "r");
+      if (f == NULL) {
+        fprintf(stderr, "failed to open file %s\n", argv[i]);
+        exit(EXIT_FAILURE);
+      }
+      Mysort(f);
+      if (fclose(f) != 0) {
+        fprintf(stderr, "failed to close file %s\n", argv[i]);
+        exit(EXIT_FAILURE);
+      }
+    }
+  }
 
   return EXIT_SUCCESS;
 }
