22a768db773bb5523fee9c89943f9ec7737908e7
zg105 <zg105@duke.edu>
2022-09-23 23:55:22 -0400

    did assignment 057

--- 057_outname/outname.c
+++ 057_outname/outname.c
@@ -1,8 +1,15 @@
+#include "outname.h"
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "outname.h"
 
 char * computeOutputFileName(const char * inputName) {
   //WRITE ME
+  // name+".counts+'\0'"
+  unsigned length = strlen(inputName) + 8;
+  char * ans = malloc(length * sizeof(*ans));
+  ans = strcpy(ans, inputName);
+  ans = strcat(ans, ".counts");
+  return ans;
 }
