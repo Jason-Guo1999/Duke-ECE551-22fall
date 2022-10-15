ded27508b025b8eed5045a4a5877529d9be7dc08
zg105 <zg105@duke.edu>
2022-10-14 23:55:36 -0400

    did

---
+++ 068_vector_ctor/vector.hpp
@@ -0,0 +1,35 @@
+/* write your class interface in this file
+   write function definitions only if they are very short
+ */
+#include <cmath>
+#include <cstdio>
+using namespace std;
+class Vector2D {
+ private:
+  double x;
+  double y;
+
+ public:
+  Vector2D() : x(0), y(0) {}
+
+  Vector2D(double _x, double _y) : x(_x), y(_y) {}
+
+  double getMagnitude() const { return sqrt(this->x * this->x + this->y * this->y); }
+
+  Vector2D operator+(const Vector2D & rhs) const {
+    Vector2D * newVector = new Vector2D();
+    newVector->x = this->x + rhs.x;
+    newVector->y = this->y + rhs.y;
+    return *newVector;
+  }
+
+  Vector2D & operator+=(const Vector2D & rhs) {
+    this->x = this->x + rhs.x;
+    this->y = this->y + rhs.y;
+    return *this;
+  }
+
+  double dot(const Vector2D & rhs) { return this->x * rhs.x + this->y * rhs.y; }
+
+  void print() const { std::printf("<%.2f, %.2f>", this->x, this->y); }
+};
