4ffa86cb47c55cabfffd650c101bc43403bdcd58
zg105 <zg105@duke.edu>
2022-10-24 00:10:36 -0400

    079

--- 079_sort_cpp/sortLines.cpp
+++ 079_sort_cpp/sortLines.cpp
@@ -6,8 +6,18 @@
 using namespace std;
 int main(int argc, char ** argv) {
   if (argc == 1) {
-    cerr << "Invalid command" << endl;
-    exit(EXIT_FAILURE);
+    string temp;
+    vector<string> input;
+    while (!cin.eof()) {
+      getline(cin, temp);
+      input.push_back(temp);
+    }
+    sort(input.begin(), input.end());
+    for (size_t i = 0; i < input.size(); i++) {
+      cout << input[i] << endl;
+    }
+    input.clear();
+    return EXIT_SUCCESS;
   }
   ifstream read;
   for (int i = 1; i < argc; i++) {
