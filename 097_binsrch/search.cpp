c62829a4a515e5cec19bb5f004f2850b4e7d5a51
zg105 <zg105@duke.edu>
2022-11-07 17:51:02 -0500

    097

---
+++ 097_binsrch/search.cpp
@@ -0,0 +1,16 @@
+#include <cstdlib>
+
+#include "function.h"
+int binarySearchForZero(Function<int, int> * f, int low, int high) {
+  while (low + 1 < high) {
+    int mid = low + ((high - low) >> 1) + 1;
+    int temp = f->invoke(mid);
+    if (temp <= 0) {
+      low = mid;
+    }
+    else {
+      high = mid - 1;
+    }
+  }
+  return low;
+}
