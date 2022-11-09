46e7fd6843db6a10d8309a73d5a65e069e83c0e0
zg105 <zg105@duke.edu>
2022-11-09 15:48:55 -0500

    099

--- 099_bst_set/bstset.h
+++ 099_bst_set/bstset.h
@@ -1,33 +1,130 @@
+
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
 #include <stdexcept>
 
-#include "bstmap.h"
-#include "map.h"
 #include "set.h"
 
-template<typename T>
-class BstSet : public Set<T> {
- private:
-  BstMap<T, int> mybst;
-
+template<typename K>
+class BstSet : public Set<K> {
  public:
-  void add(const T & key) { mybst.add(key, 0); }
+  class Node {
+   public:
+    K key;
+    Node * left;
+    Node * right;
+    Node() : key(0), left(NULL), right(NULL) {}
+    Node(const K & k) : key(k), left(NULL), right(NULL) {}
+  };
+  Node * root;
 
-  bool contains(const T & key) const {
-    bool ans;
-    try {
-      ans = mybst.lookup(key);
+  BstSet() : root(NULL) {}
+  BstSet(const BstSet & rhs) { root = copy(rhs.root); }
+
+  Node * copy(Node * target) {
+    if (target == NULL) {
+      return NULL;
     }
-    catch (std::invalid_argument & re) {
-      std::cerr << re.what() << std::endl;
-      exit(EXIT_FAILURE);
+    Node * root = new Node(target->key);
+    root->left = copy(target->left);
+    root->right = copy(target->right);
+    return root;
+  }
+  BstSet & operator=(const BstSet & rhs) {
+    if (this != &rhs) {
+      destructor(root);
+      root = copy(rhs.root);
     }
-    return ans;
+    return *this;
+  }
+  void destructor(Node * root) {
+    if (root != NULL) {
+      destructor(root->left);
+      destructor(root->right);
+      delete root;
+    }
+  }
+  virtual ~BstSet<K>() { destructor(root); }
+  virtual void add(const K & key) {
+    Node ** it = &root;
+    while (*it != NULL) {
+      if (key == (*it)->key) {
+        return;
+      }
+      else if (key < (*it)->key) {
+        it = &((*it)->left);
+      }
+      else {
+        it = &((*it)->right);
+      }
+    }
+    *it = new Node(key);
   }
 
-  void remove(const T & key) { mybst.remove(key); }
+  virtual bool contain(const K & key) const {
+    Node ** target = lookupNode(key);
+    Node * temp = *target;
+    try {
+      if (temp == NULL) {
+        throw std::invalid_argument("Can't find key!");
+      }
+    }
+    catch (std::invalid_argument & re) {
+      return false;
+    }
+    return true;
+  }
 
-  ~BstSet() {
-    mybst.destructor(mybst.root);
-    mybst.root = NULL;
+  Node ** lookupNode(const K & key) const {
+    Node ** it = const_cast<Node **>(&root);
+    while ((*it) != NULL) {
+      if ((*it)->key == key) {
+        break;
+      }
+      else if ((*it)->key > key) {
+        it = &((*it)->left);
+      }
+      else {
+        it = &((*it)->right);
+      }
+    }
+    return it;
+  }
+
+  virtual void remove(const K & key) {
+    Node ** current = &root;
+    Node * temp = NULL;
+    while (*current != NULL) {
+      if ((*current)->key == key) {
+        if ((*current)->left == NULL) {
+          temp = (*current)->right;
+          delete *current;
+          *current = temp;
+        }
+        else if ((*current)->right == NULL) {
+          temp = (*current)->left;
+          delete *current;
+          *current = temp;
+        }
+        else {
+          Node ** target = current;
+          current = &((*current)->left);
+          while ((*current)->right != NULL) {
+            current = &((*current)->right);
+          }
+          (*target)->key = (*current)->key;
+          temp = (*current)->left;
+          delete *current;
+          *current = temp;
+        }
+      }
+      else if (key < (*current)->key) {
+        current = &(*current)->left;
+      }
+      else {
+        current = &(*current)->right;
+      }
+    }
   }
 };
