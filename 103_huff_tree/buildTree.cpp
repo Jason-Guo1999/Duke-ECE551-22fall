08d01fa6af3898a32822061a95c243ccebcb3983
zg105 <zg105@duke.edu>
2022-11-21 17:38:21 -0500

    103

--- 103_huff_tree/buildTree.cpp
+++ 103_huff_tree/buildTree.cpp
@@ -1,5 +1,23 @@
 #include "node.h"
 
-Node *  buildTree(uint64_t * counts) {
+Node * buildTree(uint64_t * counts) {
   //WRITE ME!
-}  
+  priority_queue_t pqt;
+  for (size_t i = 0; i < 257; i++) {
+    if (counts[i] == 0) {
+      continue;
+    }
+    pqt.push(new Node(i, counts[i]));
+  }
+
+  while (pqt.size() >= 2) {
+    Node * temp1 = pqt.top();
+    pqt.pop();
+    Node * temp2 = pqt.top();
+    pqt.pop();
+    Node * newNode = new Node(temp1, temp2);
+    pqt.push(newNode);
+  }
+
+  return pqt.top();
+}
